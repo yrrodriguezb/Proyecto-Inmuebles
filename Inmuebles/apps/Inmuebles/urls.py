c785c86cc8cd4d706ca9e871de8465eb0d91d99f
yeison rodriguez <yrrodriguezb@gmail.com>
2017-11-08 23:55:59 -0500

    Se agrega formulario de busqueda de 'Inmuebles' en el template 'Inmueble/IListar.html'

--- Inmuebles/apps/Inmuebles/urls.py
+++ Inmuebles/apps/Inmuebles/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from .views import (
     index,
+    search,
     InmueblesListView,
     InmuebleTipoListView,
     InmuebleTipoCreateView
@@ -11,6 +12,5 @@ app_name = 'Inmuebles'
 
 urlpatterns = [
     url(r'^$', InmueblesListView.as_view(), name='inmuebles-listar'),
-    url(r'^$', InmuebleTipoListView.as_view(), name='tipo-inmuebles-listar'),
     url(r'^agregar/', InmuebleTipoCreateView.as_view(), name='tipo-inmuebles-agregar'),
 ]
--- Inmuebles/apps/Inmuebles/views.py
+++ Inmuebles/apps/Inmuebles/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.template import loader
+from django.db.models import Q
+from django.shortcuts import render_to_response
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views.generic import ListView, CreateView
@@ -24,11 +26,23 @@ class InmueblesListView(ListView):
     template_name = 'Inmueble/IListar.html'
     paginate_by = 25
 
+    def get_queryset(self):
+        query = self.request.GET.get('q', '')
+        if query:
+            qset = (
+                Q(IDescripcion__icontains=query) |
+                Q(ITipo__ITDescripcion__icontains=query) |
+                Q(ITipo__ITDescripcion_Corta__icontains=query)
+            )
+            results = Inmueble.objects.filter(qset).distinct()
+        else:
+            results = Inmueble.objects.all()
+        return results
+
 
 class InmuebleTipoListView(ListView):
     model = InmuebleTipo
     template_name = 'InmuebleTipo/ITListar.html'
-
 
     def get_context_data(self, **kwargs):
         context = super(InmuebleTipoListView, self).get_context_data(**kwargs)
